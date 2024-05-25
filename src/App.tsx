82a7cbf57d4f123a895ebd8a5e5f13ab3ad181ce
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-05-25 16:21:56 +0300

    added menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Menu Wolki</header>
+      <header className="App-header">WOLKI Grill</header>
+      <div className="App-menu">
+        <div className="App-menu-title">------ Меню ------</div>
+        <img src="./menu.jpg" alt="menu" loading="lazy" />
+        <div className="divider" />
+        <div className="App-menu-title">------ Бар ------</div>
+        <img src="./bar.jpg" alt="menu" loading="lazy" />
+      </div>
     </div>
   );
 }
