1f8392f037dc1eb345c6a68898bdfd6769e8ee83
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-05-29 15:52:44 +0300

    updated menu

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">WOLKI Grill</header>
       <div className="App-menu">
-        <div className="App-menu-title">------ Меню ------</div>
+        <div className="App-menu-title">МЕНЮ</div>
         <img src="./menu.jpg" alt="menu" loading="lazy" />
         <div className="divider" />
-        <div className="App-menu-title">------ Бар ------</div>
-        <img src="./bar.jpg" alt="menu" loading="lazy" />
+        <div className="App-menu-title">БАР</div>
+        <img src="./bar.png" alt="menu" loading="lazy" />
       </div>
     </div>
   );
