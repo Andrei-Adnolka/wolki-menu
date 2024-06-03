c95673d639b569cfa52175a09e464a4f6c009b37
Andrei Adnolka <andrei.adnolka@gmail.com>
2024-06-03 19:16:46 +0300

    updated menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import "./App.css";
+import "./App.scss";
+
+const EXTRA_DATA = [
+  { label: "Текила Olmeca silver 40мл", price: "20" },
+  { label: "Текила Olmeca gold 40мл", price: "20" },
+];
+
+const STOCK_DATA = [{ label: "Настойки", price: "20" }];
 
 function App() {
   return (
@@ -6,10 +13,33 @@ function App() {
       <header className="App-header">WOLKI Grill</header>
       <div className="App-menu">
         <div className="App-menu-title">МЕНЮ</div>
-        <img src="./menu.jpg" alt="menu" loading="lazy" />
+        <img src="./menu.png" alt="menu" loading="lazy" />
         <div className="divider" />
         <div className="App-menu-title">БАР</div>
         <img src="./bar.png" alt="menu" loading="lazy" />
+        {/* <div className="App-menu-title">ДОП</div>
+        <div className="App-menu-extra">
+          <div>
+            {EXTRA_DATA.map((el) => {
+              return (
+                <div key={el.label}>
+                  <span>{el.label}</span>
+                  <span>{el.price}</span>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            {STOCK_DATA.map((el) => {
+              return (
+                <div key={el.label}>
+                  <span>{el.label}</span>
+                  <span>{el.price}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div> */}
       </div>
     </div>
   );
